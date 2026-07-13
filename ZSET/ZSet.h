10d52a84aeb2ce1fe3a34990f0a4206480db6a74
신준규 <tlswnsrb1357@gmail.com>
2026-07-13 16:26:59 +0900

    ZSet 완성

--- ZSET/ZSet.h
+++ ZSET/ZSet.h
@@ -1,1 +1,45 @@
 #pragma once
+#include"SkipList.h"
+#include<unordered_map>
+#include<string>
+
+class ZSet
+{
+private:
+	SkipList<std::pair<int, std::string>, std::string> _sl;
+	std::unordered_map<std::string, int> _dict;
+public:
+	void add(const std::string& member, int score) {
+		auto it = _dict.find(member);
+		if (it != _dict.end()) {
+			int old = it->second;
+			if (old == score) return;
+
+			_sl.erase({ old, member });
+		}
+		_dict[member] = score;
+
+		_sl.insert({ score , member }, member);
+	}
+
+	bool score(const std::string& member, int& out) const {
+		auto it = _dict.find(member);
+		if (it == _dict.end()) {
+			return false;
+		}
+
+		out = it->second;
+		return true;
+	}
+
+	bool erase(const std::string& member) {
+		auto it = _dict.find(member);
+		if (it == _dict.end()) {
+			return false;
+		}
+		_sl.erase({ it->second, it->first });
+		_dict.erase(member);
+
+		return true;
+	}
+};
