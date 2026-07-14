f3dbb0acd637e88698858f6d5dda52a61f406035
신준규 <tlswnsrb1357@gmail.com>
2026-07-14 15:05:46 +0900

    Span을 포함한 insert 구현

--- ZSET/SkipList.h
+++ ZSET/SkipList.h
@@ -17,7 +17,7 @@ private:
 		Link* forward;
 		int level;
 
-		Node(const Key& k, const Value& v, int lvl) {
+		Node(const Key& k, const Value& v, const int lvl) {
 			key = k;
 			value = v;
 			level = lvl;
@@ -28,11 +28,11 @@ private:
 		}
 	};
 
-	Node* _head;
-	int _level;
-	int _size;
+	Node* head_;
+	int level_;
+	int size_;
 
-	int randomLevel() {
+	static int randomLevel() {
 		static std::random_device rd;
 		static std::mt19937 gen(rd());
 		static std::uniform_int_distribution<int> dis(0, 1);
@@ -44,30 +44,91 @@ private:
 	}
 public:
 	SkipList() {
-		_level = 0;
-		_size = 0;
-		_head = new Node(Key{}, Value{}, MAX_LEVEL);
+		level_ = 0;
+		size_ = 0;
+		head_ = new Node(Key{}, Value{}, MAX_LEVEL);
 	}
 	~SkipList() {
-		Node* cur = _head->forward[0];
+		Node* cur = head_->forward[0];
 		while (cur != nullptr)
 		{
 			Node* next = cur->forward[0];
 			delete cur;
 			cur = next;
 		}
-		delete _head;
+		delete head_;
 	}
 
 	Node* search(const Key& key) const {
-		Node* cur = _head;
-		for (int i = _level; i >= 0; --i) {
-			while (cur->forward[i].next != nullptr && cur->forward[i].next->key < key)
+		Node* cur = head_;
+		for (int i = level_; i >= 0; --i) 
+		{
+			while (cur->forward[i].next != nullptr 
+				&& cur->forward[i].next->key < key)
 				cur = cur->forward[i].next;
 		}
+		cur = cur->forward[0].next;
+		if (cur != nullptr && cur->key == key) return cur;
+		return nullptr;
 	}
 
 	void insert(const Key& key, const Value& value) {
+		Node* update[MAX_LEVEL + 1];
+		int rank[MAX_LEVEL + 1];
+
+		for (int i = 0; i <= MAX_LEVEL; ++i)
+		{
+			update[i] = nullptr;
+			rank[i] = 0;
+		}
+
+		Node* cur = head_;
+
+		for (int i = level_; i >= 0; --i)
+		{
+			rank[i] = (i == level_ ? 0 : rank[i + 1]);
+			while (cur->forward[i].next != nullptr 
+				&& cur->forward[i].next->key < key)
+			{
+				rank[i] += cur->forward[i].span;
+				cur = cur->forward[i].next;
+			}
+			update[i] = cur;
+		}
+
+		cur = cur->forward[0].next;
+		if (cur != nullptr && cur->key == key)
+		{
+			cur->value = value;
+			return;
+		}
+		int lvl = randomLevel();
+		if (lvl > level_)
+		{
+			for (int i = level_ + 1; i <= lvl; ++i)
+			{
+				rank[i] = 0;
+				update[i] = head_;
+				head_->forward[i].span = size_;
+			}
+			level_ = lvl;
+		}
+		Node* n = new Node(key, value, lvl);
+
+		for (int i = 0; i <= lvl; ++i)
+		{
+			n->forward[i].next = update[i]->forward[i].next;
+			update[i]->forward[i].next = n;
+			
+			n->forward[i].span = update[i]->forward[i].span - (rank[0] - rank[i]);
+			update[i]->forward[i].span = rank[0] - rank[i] + 1;
+		}
+
+		for (int i = lvl; i <= level_; ++i)
+		{
+			update[i]->forward[i].span += 1;
+		}
+		size_++;
 	}
 
 	bool erase(const Key& key) {
@@ -76,6 +137,6 @@ public:
 	void rangeSearch(Key& k1, Key& k2, std::vector<Node*>& v) {
 	}
 
-	int size() { return _size; }
-	bool empty() { return _size == 0; }
+	int size() { return size_; }
+	bool empty() { return size_ == 0; }
 };
