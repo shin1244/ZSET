2d079de38f6a76e9493ff19594762ddaa7bc6d13
신준규 <tlswnsrb1357@gmail.com>
2026-07-16 12:00:35 +0900

    오브젝트 풀 완성

--- ObjectPool/ObjectPool .h
+++ ObjectPool/ObjectPool .h
@@ -7,8 +7,8 @@ template<typename T, size_t N>
 class ObjectPool
 {
 private:
-	array<T, N> pool_;
-	vector<size_t> free_;
+	std::array<T, N> pool_;
+	std::vector<size_t> free_;
 public:
 	ObjectPool()
 	{
@@ -18,11 +18,17 @@ public:
 	}
 	T* allocate()
 	{
-		
-		return nullptr;
+		if (free_.empty()) return nullptr;
+		size_t idx = free_.back();
+		free_.pop_back();
+		return &pool_[idx];
 	}
 	void deallocate(T* obj)
 	{
-		
+		const size_t idx = obj - pool_.data();
+		free_.push_back(idx);
 	}
+	size_t available() const { return free_.size(); }  
+	static size_t capacity() { return N; }
+	bool   empty()    const { return free_.empty(); } 
 };
